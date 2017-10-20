7fc163777e8254e1774cfb9d3745266fae6996c0
ElectiwirezMusic <electiwirez@saratogachess.org>
2017-10-19 19:10:34 -0700

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -4,15 +4,36 @@
 //
 //Class Description: implements palindrome logic  
 public class Palindrome { 
+	String str ;
 	
-	public static boolean isPal(String str) {
-        return str.length() <= 1 ||(str.charAt(0) == str.charAt(str.length() - 1) && isPal(str.substring(1, str.length() - 1)));
+	public Palindrome(String palstr)
+	{
+		str = stripLower(palstr) ;
+	}
+	
+	public boolean isPalindrome() {
+		if (!validate(str))
+			return false ;
+		if (str.length() == 1)
+			return false ;
+		return isPal(str) ;
+	}
+	
+	private boolean isPal(String str)
+	{
+		if (str.length() < 2)
+			return true ;
+		if ( str.charAt(0) != str.charAt(str.length() - 1) )
+				return false ;
+		else
+				return isPal(str.substring(1, str.length() - 1));
     }
 
 
     public String stripLower(String str) {
         return  (str.replaceAll("[^A-Za-z0-9]", "")).toLowerCase();
     }
+    
 	public boolean validate(String str) {
 		char charArray[] = str.toCharArray();
 		
--- PalindromeTester.java
+++ PalindromeTester.java
@@ -1,39 +1,28 @@
 //import statements 
-import java.lang.Character;
 import java.util.Scanner;
 //driver for Palindrome
 public class PalindromeTester {
-	
-	private static Scanner input;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Palindrome Program! Enter the letter q when you are done.");
-		
-        Palindrome pal = new Palindrome();
-        String str;
+         
+        Scanner input = new Scanner(System.in);
         
-        do{		
-        
-			System.out.println("Enter a string: "); 
-			Scanner input = new Scanner(System.in);
-			
-			str = input.nextLine();
-			
-			String str2 = str;
-			
-			str2 = pal.stripLower(str);
-			
-			if(str.length() == 1){
-					if(str.equals("q"))
-						break;
-				System.out.println(str + " is not a palindrome");
-			}					
-			else if(pal.isPal(str2) == true)
+        System.out.print("Enter a string: "); 
+		String str = input.nextLine();
+        while ( !str.equals("q") )	
+        {
+        	Palindrome pal = new Palindrome(str);
+				
+        	if( pal.isPalindrome() )  // true
 			    System.out.println(str + " is a palindrome");
-			else
+			else					// false
 			    System.out.println(str + " is not a palindrome");
 
-		 
-        } while(!(str.equals("q")));
+        	System.out.print("Enter a string: "); 
+			str = input.nextLine();
+		}
+        System.out.println("Exiting..."); 
+        input.close();
 	}
 }
