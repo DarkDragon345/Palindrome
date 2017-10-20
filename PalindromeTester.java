32f7b2c442301067b36c90697f0f20c2539960ea
ElectiwirezMusic <electiwirez@saratogachess.org>
2017-10-19 19:27:26 -0700

    Update PalindromeTester.java

--- PalindromeTester.java
+++ PalindromeTester.java
@@ -1,17 +1,29 @@
+//Name: Tushar Shrivastav
+//
+//File Name: PalindromeTester.java
+//
+//Class Description: Driver class which will execute the logic in Palindrome.java
+
 //import statements 
 import java.util.Scanner;
 //driver for Palindrome
 public class PalindromeTester {
-
+	
+	//main method
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Palindrome Program! Enter the letter q when you are done.");
+	System.out.println("Welcome to the Palindrome Program! Enter the letter q when you are done.");
          
+	//creating new Scanner object
         Scanner input = new Scanner(System.in);
         
+	//ask for input
         System.out.print("Enter a string: "); 
-		String str = input.nextLine();
-        while ( !str.equals("q") )	
+	String str = input.nextLine();
+
+	//while the string in the input is NOT q keep asking for more strings
+	while ( !str.equals("q") )	
         {
+		//creating new Palindrome object
         	Palindrome pal = new Palindrome(str);
 				
         	if( pal.isPalindrome() )  // true
