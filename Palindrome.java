451225da9fe51e4b8a691d439754748292cd4485
ElectiwirezMusic <electiwirez@saratogachess.org>
2017-10-19 19:18:40 -0700

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -5,20 +5,20 @@
 //Class Description: implements palindrome logic  
 public class Palindrome { 
 	String str ;
-	
+
+	//Constructor
 	public Palindrome(String palstr)
 	{
 		str = stripLower(palstr) ;
 	}
-	
+	//public method to check if the object palindrome is a palindrome or not
 	public boolean isPalindrome() {
-		if (!validate(str))
-			return false ;
 		if (str.length() == 1)
 			return false ;
 		return isPal(str) ;
 	}
-	
+	//private method for class to check if the string initialized during the 
+	//constructor is a palindrome or not.
 	private boolean isPal(String str)
 	{
 		if (str.length() < 2)
@@ -26,23 +26,11 @@ public class Palindrome {
 		if ( str.charAt(0) != str.charAt(str.length() - 1) )
 				return false ;
 		else
-				return isPal(str.substring(1, str.length() - 1));
-    }
+			return isPal(str.substring(1, str.length() - 1));
+	}
 
-
-    public String stripLower(String str) {
-        return  (str.replaceAll("[^A-Za-z0-9]", "")).toLowerCase();
-    }
-    
-	public boolean validate(String str) {
-		char charArray[] = str.toCharArray();
-		
-		for (int i = 0; i < charArray.length; i++){
-			if ((charArray[i] >= 'a' && charArray[i] <= 'z') || (charArray[i] >= '0' && charArray[i] <= '9'))
-				continue;
-			else 
-				return false;
-		}
-		return true;
+	//removes all characters not non-alphanumeric characters AND converts the string into lowercase.
+	public String stripLower(String str) {
+		return	(str.replaceAll("[^A-Za-z0-9]", "")).toLowerCase();
 	}
 }
